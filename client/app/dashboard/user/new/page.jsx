7e0b3eea506edc85c446d97f75f02f07d3cbaa28
DangLam <tadanglam93@gmail.com>
2024-05-25 08:46:24 +0700

    Update New && Update User Page

--- client/app/dashboard/user/new/page.jsx
+++ client/app/dashboard/user/new/page.jsx
@@ -1,15 +1,71 @@
 'use client'
 import Link from "next/link";
 import { useState } from "react";
+import { MdOutlineToday } from "react-icons/md";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format } from "date-fns";
+import Swal from "sweetalert2";
+import { useRouter } from "next/navigation";
+
+import { Register } from "@/app/api/route";
 
 
 const NewUser = () => {
+    const router = useRouter();
+    const [username, setUserName] = useState('');
+    const [fullname, setFullName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmps, setConfirmps] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [sex, setSex] = useState('male');
+    const [dateOfBirth, setDateOfBirth] = useState(null);
+    const [loading, setLoading] = useState(false);
 
-    const saveForm = async(e) => {
+    const handleRegister = async(e) => {
         e.preventDefault();
+        try{
+            const formatDate = format(new Date(dateOfBirth), 'yyyy-MM-dd');
+
+            const bodyFormData = new FormData();
+            bodyFormData.append('username', username);
+            bodyFormData.append('fullname', fullname);
+            bodyFormData.append('password', password);
+            bodyFormData.append('confirmps', confirmps);
+            bodyFormData.append('email', email);
+            bodyFormData.append('phone', phone);
+            bodyFormData.append('sex', sex);
+            bodyFormData.append('dateOfBirth', formatDate);
+
+            await Register(bodyFormData);
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.onmouseenter = Swal.stopTimer;
+                  toast.onmouseleave = Swal.resumeTimer;
+                }
+                
+              });
+              Toast.fire({
+                icon: "success",
+                title: "Signup is successfully"
+              });
+            router.push('/dashboard/user');
+        }catch(error){
+            Swal.fire({
+                icon: 'error',
+                text: `${error.response.data.error}`,
+            });
+        }
     }
 
-    const [loading, setLoading] = useState(false);
+
+    
     return ( 
         <div>
         {loading ? (
@@ -19,15 +75,15 @@ const NewUser = () => {
         ) : (
             <div className="flex flex-col gap-2">
                 <div className="flex item-center gap-4">
-                    <Link href={'/dashboard/user'} className="text-[#4b6cb7] text-3xl mb-5 font-bold">User</Link>
+                    <Link href={'/dashboard/user'} className="text-[#4b6cb7] text-3xl mb-5 font-bold hover:opacity-70 duration-300">User</Link>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="flex item-center justify-center w-6 h-9">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
                     </svg>
                     <Link href={`/dashboard/user/new`} className="text-[#4b6cb7] text-3xl mb-5 hover:text-btn font-bold">New</Link>
                 </div>
-                <form onSubmit={saveForm} encType="multipart/form-data" className="flex flex-col gap-8 w-full h-full">
+                <form onSubmit={handleRegister} encType="multipart/form-data" className="flex flex-col gap-8 w-full h-full">
                     <div className=" flex flex-col w-full h-1/6">
-                        <label className="text-[#4b6cb7] font-semibold" htmlFor="ProductName">
+                        <label className="text-[#4b6cb7] font-semibold" >
                             Full Name
                         </label>
                         <input
@@ -35,14 +91,29 @@ const NewUser = () => {
                             type="text"
                             placeholder="Please enter full name...."
                             className="p-1.5 rounded-lg"
-                            // value={name}
-                            // onChange={(e) => setName(e.target.value)}
+                            value={fullname}
+                            onChange={(e) => setFullName(e.target.value)}
                         />
                     </div>
             
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold" >
+                            Sex
+                        </label>
+                        <select
+                            id="user"
+                            type="text"
+                            className="p-1.5 rounded-lg"
+                            value={sex}
+                            onChange={(e) => setSex(e.target.value)}
+                        >
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
             
                     <div className=" flex flex-col w-full h-1/6">
-                        <label className="text-[#4b6cb7] font-semibold" htmlFor="ProductName">
+                        <label className="text-[#4b6cb7] font-semibold" >
                             User Name
                         </label>
                         <input
@@ -50,48 +121,84 @@ const NewUser = () => {
                             type="text"
                             placeholder="Please enter user name...."
                             className="p-1.5 rounded-lg"
-                            // value={desc}
-                            // onChange={(e) => setDesc(e.target.value)}
+                            value={username}
+                            onChange={(e) => setUserName(e.target.value)}
                         >
                         </input>
                     </div>
 
-
-                    {/* <div className="flex  gap-4  w-full h-1/6">
-                        <label
-                            // onChange={handleImageChange}
-                            className="border w-32 h-32 text-center flex items-center justify-center rounded-lg bg-gray-200 cursor-pointer text-[#4b6cb7] hover:bg-[#4b6cb7] duration-500 hover:text-white"
-                        >
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="w-6 h-6"
-                            >
-                                <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-                                />
-                            </svg>
-                            <div>Upload</div>
-                            <input type="file" className="hidden" multiple />
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold">
+                            Password
                         </label>
-                        {!!images && images.length > 0 && (
-                            <div className="flex items-center">
-                                You Have {images.length} photos
-                            </div>
-                        )}
-
-                    </div> */}
-            
-            
-                    <div className="flex flex-col gap-2 w-full h-1/6">
-
+                        <input
+                            id="password"
+                            type="password"
+                            placeholder="Please enter password...."
+                            className="p-1.5 rounded-lg"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        >
+                        </input>
                     </div>
-            
+
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold">
+                            Confirm Password
+                        </label>
+                        <input
+                            id="confirmpassword"
+                            type="password"
+                            placeholder="Please enter confirm password...."
+                            className="p-1.5 rounded-lg"
+                            value={confirmps}
+                            onChange={(e) => setConfirmps(e.target.value)}
+                        >
+                        </input>
+                    </div>
+
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold">
+                            Email
+                        </label>
+                        <input
+                            id="email"
+                            type="text"
+                            placeholder="Please enter email...."
+                            className="p-1.5 rounded-lg"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        >
+                        </input>
+                    </div>
+
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold" >
+                            Phone
+                        </label>
+                        <input
+                            id="phone"
+                            type="text"
+                            placeholder="Please enter phone...."
+                            className="p-1.5 rounded-lg"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        >
+                        </input>
+                    </div>
+
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold">
+                            Date Of Birth
+                        </label>
+                        <DatePicker 
+                            selected={dateOfBirth} 
+                            onChange={(date) => setDateOfBirth(date)}
+                            placeholderText="Select Date Of Birth..."
+                            className="p-1.5 rounded-lg w-full"
+                        />
+                    </div>
+
                     <button type="submit" className="text-white bg-[#4b6cb7] hover:bg-blue-900 py-1 rounded-md w-1/12  h-1/6">
                         Save
                     </button>
--- client/app/dashboard/user/update/[id]/page.jsx
+++ client/app/dashboard/user/update/[id]/page.jsx
@@ -1,117 +1,230 @@
 'use client'
 import Link from "next/link";
+import { useEffect, useState } from "react";
+import { MdOutlineToday } from "react-icons/md";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { format } from "date-fns";
+import Swal from "sweetalert2";
 import { useParams, useRouter } from "next/navigation";
-import { useState } from "react";
+
+import { updateUser, getDetailUser } from "@/app/api/route";
 
 const UpdateUser = () => {
+    const router = useRouter();
     const { id } = useParams();
-    const [user, setUser] = useState();
+    const accessToken = sessionStorage.getItem('accessToken');
+    const [username, setUserName] = useState('');
+    const [fullname, setFullName] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmps, setConfirmps] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
+    const [sex, setSex] = useState('male');
+    const [dateOfBirth, setDateOfBirth] = useState(null);
     const [loading, setLoading] = useState(false);
 
-    const saveForm  = async(e) => {
+    const fetchDetailUser = async(id) => {
+        try {
+            const response = await getDetailUser(id, accessToken);
+            setFullName(response.fullname);
+            setUserName(response.username);
+            setPassword(response.password);
+            setConfirmps(response.confirmps);
+            setEmail(response.email);
+            setPhone(response.phone);
+            setSex(response.sex);
+            setDateOfBirth(response.dateOfBirth);
+        } catch (error) {
+            console.log('Error fetch user detail: ', error);
+        }
+    }
+
+    useEffect(() => {
+        fetchDetailUser(id)
+    }, [id])
+
+    const handleUpdate = async(e) => {
         e.preventDefault();
+        try{
+            const formatDate = format(new Date(dateOfBirth), 'yyyy-MM-dd');
+
+            const bodyFormData = new FormData();
+            bodyFormData.append('username', username);
+            bodyFormData.append('fullname', fullname);
+            bodyFormData.append('password', password);
+            bodyFormData.append('confirmps', confirmps);
+            bodyFormData.append('email', email);
+            bodyFormData.append('phone', phone);
+            bodyFormData.append('sex', sex);
+            bodyFormData.append('dateOfBirth', formatDate);
+
+            await updateUser(bodyFormData);
+            const Toast = Swal.mixin({
+                toast: true,
+                position: "top-end",
+                showConfirmButton: false,
+                timer: 3000,
+                timerProgressBar: true,
+                didOpen: (toast) => {
+                  toast.onmouseenter = Swal.stopTimer;
+                  toast.onmouseleave = Swal.resumeTimer;
+                }
+                
+              });
+              Toast.fire({
+                icon: "success",
+                title: "Signup is successfully"
+              });
+            router.push('/dashboard/user');
+        }catch(error){
+            Swal.fire({
+                icon: 'error',
+                text: `${error.response.data.error}`,
+            });
+        }
     }
 
     return ( 
         <div>
-            {loading ? (
-                <div className="text-center py-40">
+        {loading ? (
+            <div className="text-center py-40">
                 Loading......
+            </div>
+        ) : (
+            <div className="flex flex-col gap-2">
+                <div className="flex item-center gap-4">
+                    <Link href={'/dashboard/user'} className="text-[#4b6cb7] text-3xl mb-5 font-bold hover:opacity-70 duration-300">User</Link>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="flex item-center justify-center w-6 h-9">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
+                    </svg>
+                    <Link href={`/dashboard/user/new`} className="text-[#4b6cb7] text-3xl mb-5 hover:text-btn font-bold">Update</Link>
                 </div>
-            ) : (
-                <div className="flex flex-col gap-2">
-                    <div className="flex item-center gap-4">
-                        <Link href={'/dashboard/user'} className="text-[#4b6cb7] text-3xl mb-5 hover:text-btn font-bold">User</Link>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="flex item-center justify-center w-6 h-9">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 4.5l7.5 7.5-7.5 7.5m-6-15l7.5 7.5-7.5 7.5" />
-                        </svg>
-                        <Link href={`/dashboard/user/${id}`} className="text-[#4b6cb7] text-3xl mb-5 hover:text-btn font-bold">Update</Link>
+                <form onSubmit={handleUpdate} encType="multipart/form-data" className="flex flex-col gap-8 w-full h-full">
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold" >
+                            Full Name
+                        </label>
+                        <input
+                            id="user"
+                            type="text"
+                            placeholder="Please enter full name...."
+                            className="p-1.5 rounded-lg"
+                            value={fullname}
+                            onChange={(e) => setFullName(e.target.value)}
+                        />
                     </div>
-                    <form onSubmit={saveForm} encType="multipart/form-data" className="flex flex-col gap-8 w-full h-full">
-                        <div className=" flex flex-col w-full h-1/6">
-                            <label className="text-[#4b6cb7] font-semibold" htmlFor="ProductName">
-                                User
-                            </label>
-                            <input
-                                id="user"
-                                type="text"
-                                placeholder="Product Name...."
-                                className="p-1.5 rounded-lg"
-                                // value={name}
-                                // onChange={(e) => setName(e.target.value)}
-                            />
-                        </div>
-                
-                
-                        <div className=" flex flex-col w-full h-1/6">
-                            <label className="text-[#4b6cb7] font-semibold" htmlFor="ProductName">
-                                Description
-                            </label>
-                            <textarea
-                                id="Description"
-                                placeholder="Description...."
-                                className="p-1.5 rounded-lg"
-                                // value={desc}
-                                // onChange={(e) => setDesc(e.target.value)}
-                            >
-                            </textarea>
-                        </div>
+            
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold" >
+                            Sex
+                        </label>
+                        <select
+                            id="user"
+                            type="text"
+                            className="p-1.5 rounded-lg"
+                            value={sex}
+                            onChange={(e) => setSex(e.target.value)}
+                        >
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
+            
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold" >
+                            User Name
+                        </label>
+                        <input
+                            id="username"
+                            type="text"
+                            placeholder="Please enter user name...."
+                            className="p-1.5 rounded-lg"
+                            value={username}
+                            onChange={(e) => setUserName(e.target.value)}
+                        >
+                        </input>
+                    </div>
 
-                        {/* <div className="flex  gap-4  w-full h-1/6">
-                            <label
-                                // onChange={handleImageChange}
-                                className="border w-32 h-32 text-center flex items-center justify-center rounded-lg bg-gray-200 cursor-pointer text-[#4b6cb7] hover:bg-[#4b6cb7] duration-500 hover:text-white"
-                            >
-                                <svg
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    fill="none"
-                                    viewBox="0 0 24 24"
-                                    strokeWidth={1.5}
-                                    stroke="currentColor"
-                                    className="w-6 h-6"
-                                >
-                                    <path
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
-                                    />
-                                </svg>
-                                <div>Upload</div>
-                                <input type="file" className="hidden" multiple />
-                            </label>
-                            {!!images && images.length > 0 && (
-                                <div className="flex items-center">
-                                    You Have {images.length} photos
-                                </div>
-                            )}
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold">
+                            Password
+                        </label>
+                        <input
+                            id="password"
+                            type="password"
+                            placeholder="Please enter password...."
+                            className="p-1.5 rounded-lg"
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                        >
+                        </input>
+                    </div>
 
-                        </div> */}
-                
-                        <div className=" flex flex-col w-full h-1/6">
-                            <label className="text-[#4b6cb7] font-semibold" htmlFor="Productprice">
-                                Price (in USD)
-                            </label>
-                            <input
-                                id="Productprice"
-                                type="number"
-                                placeholder="Price..."
-                                className="p-1.5 rounded-lg"
-                                // value={price}
-                                // onChange={(e) => setPrice(e.target.value)}
-                            />
-                        </div>
-                
-                        <div className="flex flex-col gap-2 w-full h-1/6">
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold">
+                            Confirm Password
+                        </label>
+                        <input
+                            id="confirmpassword"
+                            type="password"
+                            placeholder="Please enter confirm password...."
+                            className="p-1.5 rounded-lg"
+                            value={confirmps}
+                            onChange={(e) => setConfirmps(e.target.value)}
+                        >
+                        </input>
+                    </div>
 
-                        </div>
-                
-                        <button type="submit" className="text-white bg-[#4b6cb7] hover:bg-blue-900 py-1 rounded-md w-1/12  h-1/6">
-                            Save
-                        </button>
-                    </form>
-                </div>
-            )}
-        </div>
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold">
+                            Email
+                        </label>
+                        <input
+                            id="email"
+                            type="text"
+                            placeholder="Please enter email...."
+                            className="p-1.5 rounded-lg"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                        >
+                        </input>
+                    </div>
+
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold" >
+                            Phone
+                        </label>
+                        <input
+                            id="phone"
+                            type="text"
+                            placeholder="Please enter phone...."
+                            className="p-1.5 rounded-lg"
+                            value={phone}
+                            onChange={(e) => setPhone(e.target.value)}
+                        >
+                        </input>
+                    </div>
+
+                    <div className=" flex flex-col w-full h-1/6">
+                        <label className="text-[#4b6cb7] font-semibold">
+                            Date Of Birth
+                        </label>
+                        <DatePicker 
+                            selected={dateOfBirth} 
+                            onChange={(date) => setDateOfBirth(date)}
+                            placeholderText="Select Date Of Birth..."
+                            className="p-1.5 rounded-lg w-full"
+                        />
+                    </div>
+
+                    <button type="submit" className="text-white bg-[#4b6cb7] hover:bg-blue-900 py-1 rounded-md w-1/12  h-1/6">
+                        Save
+                    </button>
+                </form>
+            </div>
+        )}
+    </div>
     );
 }
  
